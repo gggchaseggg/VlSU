b7674322eb89ee45cd030840f875cc47454ab46d
gggchaseggg <gggchaseggg@gmail.com>
2021-04-08 00:00:04 +0300

    +бин поиск

--- AiSD/LAB3/lab3_1.py
+++ AiSD/LAB3/lab3_1.py
@@ -1,22 +1,25 @@
 import random
 
-def bin_poisk(arr,key):
-    if len(arr) == 1:
-        if arr[0] == key:
+def bin_poisk(key,left=0,right=15):
+    if (right-left) <= 1:
+        if a[left] == key:
             print ("Присутствует!")
         else:
-            print ("Не присутствует!")
+            print ("Не присутствует!\nВставить его нужно на {} место".format(right))
+            a.insert(right,key)
+            print(a)
+
         return
     
-    if key < arr[len(arr)//2]:
-        bin_poisk(arr[:(len(arr)//2)],key)
+    if key < a[(right-left)//2+left]:
+        bin_poisk(key,left,((right-left)//2)+left)
     else:
-        bin_poisk(arr[(len(arr)//2):],key)
+        bin_poisk(key,((right-left)//2)+left,right)
 
 a = [random.randint(-50,50) for i in range(15)]
 a.sort()
 print (a)
 
-key = int(input("Введите ключ:"))
+key = int(input("Введите ключ: "))
 
-bin_poisk(a,key)
+bin_poisk(key)
---
+++ AiSD/LAB3/lab3_1_1.py
@@ -0,0 +1,23 @@
+import random
+
+def bin_poisk(arr,key):
+    if len(arr) == 1:
+        if arr[0] == key:
+            print ("Присутствует!")
+        else:
+            print ("Не присутствует!")
+            
+        return
+    
+    if key < arr[len(arr)//2]:
+        bin_poisk(arr[:(len(arr)//2)],key)
+    else:
+        bin_poisk(arr[(len(arr)//2):],key)
+
+a = [random.randint(-50,50) for i in range(15)]
+a.sort()
+print (a)
+
+key = int(input("Введите ключ: "))
+
+bin_poisk(a,key)
