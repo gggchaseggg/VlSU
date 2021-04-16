8c2a94e0cb04d1788a7a2b33925c79cd6e77da5e
gggchaseggg <gggchaseggg@gmail.com>
2021-04-16 16:59:02 +0300

    3 лаба АиСД

--- 2 semestr/AiSD/LAB3/lab3_1.py
+++ 2 semestr/AiSD/LAB3/lab3_1.py
@@ -1,6 +1,33 @@
 import random
 
+def menu():
+    print ('''
+    1 - На каком месте стоит элемент
+    2 - К-статистика
+    ''')
+
+    x = int(input("Выберите пункт: "))
+    
+    if x == 1:
+        a.sort()
+        print (a)
+        key = int(input("Введите ключ: "))
+        bin_poisk(key)
+    elif x == 2:
+        print (a)
+        ks(a)
+
+def ks(array):
+    key = int(input("Введите значение: "))
+    for i in range(len(array)):
+        if array[i] == key:
+            print ("Элемент стоит на {} месте в неотсортированном множетсве".format(i))
+            return
+    else:
+        print ("Такого элемента нет")
+
 def bin_poisk(key,left=0,right=15):
+
     if (right-left) <= 1:
         if a[left] == key:
             print ("Присутствует!")
@@ -17,9 +44,4 @@ def bin_poisk(key,left=0,right=15):
         bin_poisk(key,((right-left)//2)+left,right)
 
 a = [random.randint(-50,50) for i in range(15)]
-a.sort()
-print (a)
-
-key = int(input("Введите ключ: "))
-
-bin_poisk(key)
+menu()
