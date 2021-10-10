ac760e039fd4d441dee55e4ca798ca2b402c0869
gggchaseggg <gggchaseggg@gmail.com>
2021-10-10 19:03:44 +0300

    chto-to

---
+++ 3 semestr/TP/lab3/Port/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Port
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    abstract class Sudno
+    {
+        public string name;
+        public int vodoizmesh;
+        public int ves;
+
+        public abstract void GetInfo();
+    }
+
+    class Passag : Sudno
+    {
+        public int vmestimost;
+        public string marshrut;
+
+        public override void GetInfo()
+        {
+            Console.WriteLine("Название судна : " + name);
+            Console.WriteLine("Водоизмещение : " + vodoizmesh);
+            Console.WriteLine("Вес судна : " + ves);
+        }
+    }
+}
