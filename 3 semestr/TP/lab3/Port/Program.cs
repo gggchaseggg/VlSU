550e19de5f15c19b4e185b2f2112de47cebc11fb
gggchaseggg <gggchaseggg@gmail.com>
2021-11-13 17:59:48 +0300

    13.11.2021

--- 3 semestr/TP/lab3/Port/Program.cs
+++ 3 semestr/TP/lab3/Port/Program.cs
@@ -6,7 +6,9 @@ namespace Port
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Passag korabl = new Passag();
+            korabl.InputInfo();
+            korabl.PrintInfo();
         }
     }
 
@@ -16,19 +18,43 @@ namespace Port
         public int vodoizmesh;
         public int ves;
 
-        public abstract void GetInfo();
+        public abstract void PrintInfo();
+        public abstract void InputInfo();
     }
 
     class Passag : Sudno
     {
         public int vmestimost;
         public string marshrut;
+        private string sityfrom, sityto;
 
-        public override void GetInfo()
+        private void dlinamarshruta ()
+        {
+        //do something
+        }
+
+        public override void PrintInfo()
         {
             Console.WriteLine("Название судна : " + name);
             Console.WriteLine("Водоизмещение : " + vodoizmesh);
             Console.WriteLine("Вес судна : " + ves);
+            Console.WriteLine("Вместимость людей : " + vmestimost);
+            Console.WriteLine("Маршрут : " + marshrut);
         }
+
+        public override void InputInfo()
+        {
+            Console.Write("\nВведите название судна: ");
+            name = Console.ReadLine();
+            Console.Write("Введите водоизмещение судна: ");
+            vodoizmesh = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите вес судна: ");
+            ves = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите вместимость судна: ");
+            vmestimost = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите маршрут судна: ");
+            marshrut = Console.ReadLine();
+        }
+
     }
 }
