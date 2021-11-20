99bc99e48b51cd3c035605572e0157d2bca902ac
gggchaseggg <gggchasegg@gmail.com>
2021-11-20 13:08:53 +0300

    +lab3_tp

--- 3 semestr/TP/lab3/Port/Program.cs
+++ 3 semestr/TP/lab3/Port/Program.cs
@@ -6,54 +6,131 @@ namespace Port
     {
         static void Main(string[] args)
         {
-            Passag korabl = new Passag();
-            korabl.InputInfo();
-            korabl.PrintInfo();
+            
+            Passenger[] Spisok = new Passenger[7];
+
+            Spisok[0] = new Passenger("Бригантина",520,1540000,330);
+            Spisok[1] = new Passenger("Победа", 100, 120000, 100);
+            Spisok[2] = new Passenger("Беда", 10, 15400, 0);
+
+            byte LastInd = 3;
+
+            Console.WriteLine("Здравствуйте!");
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Какое действие вы хотите выполнить?");
+                Console.WriteLine("1 - Добавить судно и информацию о нем");
+                Console.WriteLine("2 - Получить информацию о судне");
+                Console.WriteLine("3 - Добавить пассажиров на судно");
+                Console.WriteLine("4 - закрыть программу");
+                Console.Write("Выберите пункт: ");
+
+                byte key = Convert.ToByte(Console.ReadLine());
+                switch (key)
+                {
+                    case 1:
+                        if (LastInd == 6)
+                        {
+                            Console.WriteLine("НЕ ОСТАЛОСЬ МЕСТА В ПОРТУ");
+                            break;
+                        }
+
+                        Console.WriteLine("--------------------------------------------------");
+                        Console.Write("Введите название судна: ");
+                        string name = Console.ReadLine();
+                        
+                        Console.Write("Введите вместимость судна: ");
+                        int vmest = Convert.ToInt32(Console.ReadLine());
+                        
+                        Console.Write("Введите вес судна: ");
+                        int ves = Convert.ToInt32(Console.ReadLine());
+                        
+                        Console.Write("Введите кол-во людей на судне: ");
+                        int pasamount = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("--------------------------------------------------");
+                        
+                        Spisok[LastInd] = new Passenger(name, vmest, ves, pasamount);
+                        Console.WriteLine("Было создано судно с индексом - " + LastInd);
+                        LastInd++;
+
+                        break;
+
+                    case 2:
+                        Console.Write("Введите индекс судна: ");
+                        byte ind = Convert.ToByte(Console.ReadLine());
+                        Spisok[ind].PrintInfo();
+                        break;
+
+                    case 3:
+                        Console.Write("Введите индекс судна: ");
+                        byte ind1 = Convert.ToByte(Console.ReadLine());
+                        Console.Write("Введите количество севших пассажиров: ");
+                        int plssize = Convert.ToInt32(Console.ReadLine());
+                        Spisok[ind1].PlusPassenger(plssize);
+                        break;
+
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+            
+            
         }
     }
 
     abstract class Sudno
     {
-        public string name;
-        public int vodoizmesh;
-        public int ves;
+        protected string Name;
+        protected int Vmest;
+        protected int Ves;
+
+        protected Sudno(string name, int vmest, int ves)
+        {
+            Name = name;
+            Vmest = vmest;
+            Ves = ves;
+        }
 
         public abstract void PrintInfo();
-        public abstract void InputInfo();
     }
 
-    class Passag : Sudno
+    class Passenger : Sudno
     {
-        public int vmestimost;
-        public string marshrut;
-        private string sityfrom, sityto;
+        private int PasAmount;
 
-        private void dlinamarshruta ()
+        public Passenger(string name, int vmest, int ves, int pasamount) : base(name, vmest, ves)
         {
-        //do something
+            PasAmount = pasamount;
+        }
+
+        private void Zagruzhen()
+        {
+            if (PasAmount == Vmest)
+            {
+                Console.WriteLine("{0} - Уже заполнено ", Name);
+            }
+            else
+            {
+                Console.WriteLine("{0} - Не заполнено или переполнено ", Name);
+            }
         }
 
         public override void PrintInfo()
         {
-            Console.WriteLine("Название судна : " + name);
-            Console.WriteLine("Водоизмещение : " + vodoizmesh);
-            Console.WriteLine("Вес судна : " + ves);
-            Console.WriteLine("Вместимость людей : " + vmestimost);
-            Console.WriteLine("Маршрут : " + marshrut);
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Название судна : " + Name);
+            Console.WriteLine("Вместимость людей : " + Vmest);
+            Console.WriteLine("Вес судна : " + Ves);
+            Console.WriteLine("Находящиеся на борту люди : " + PasAmount);
+            Zagruzhen();
+            Console.WriteLine("--------------------------------------------------");
         }
 
-        public override void InputInfo()
+        public void PlusPassenger(int plus_size)
         {
-            Console.Write("\nВведите название судна: ");
-            name = Console.ReadLine();
-            Console.Write("Введите водоизмещение судна: ");
-            vodoizmesh = Int32.Parse(Console.ReadLine());
-            Console.Write("Введите вес судна: ");
-            ves = Int32.Parse(Console.ReadLine());
-            Console.Write("Введите вместимость судна: ");
-            vmestimost = Int32.Parse(Console.ReadLine());
-            Console.Write("Введите маршрут судна: ");
-            marshrut = Console.ReadLine();
+            PasAmount += plus_size;
         }
 
     }
