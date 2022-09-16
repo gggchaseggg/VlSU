0f8caa22c84af8440a69201a0ae31bccf3057110
Daniil <gggchaseggg@gmail.com>
2022-09-16 18:16:57 +0300

    tp-1lab()no-full

--- RPS(Java)/lab1/ru/vlsu/ispi/SampleServlet.java
+++ RPS(Java)/lab1/ru/vlsu/ispi/SampleServlet.java
@@ -6,10 +6,10 @@ import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
