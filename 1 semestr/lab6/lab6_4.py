3844cdde1262e1f6f91766d9c51c0151bbcde8cd
Daniil <gggchaseggg@gmail.com>
2023-01-12 12:10:51 +0300

    set-only-3semestr

--- 1 semestr/lab6/lab6_4.py
+++
@@ -1,9 +0,0 @@
-def element(x):
-    a = [0,1]
-    for i in range(2,x+1):
-        a.append(a[i-2]+a[i-1])
-    return a[x]
-k = int(input("Введите k-тое число: "))
-n = int(input("Введите n-тое число: "))
-s = element(k)/element(n)
-print ("Отношение k/n:", round(s,3))
