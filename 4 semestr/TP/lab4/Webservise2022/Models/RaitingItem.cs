aad84e531865a6bc30eb9c86be7e054a18bc9cb7
Daniil <gggchaseggg@gmail.com>
2023-01-12 12:09:34 +0300

    set-only-2semestr

--- 4 semestr/TP/lab4/Webservise2022/Models/RaitingItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Webservise2022.Models
-{
-    public class RaitingItem
-    {
-        public long Id { get; set; }
-        public string? Name { get; set; }
-        public int Grade { get; set; }
-    }
-}
