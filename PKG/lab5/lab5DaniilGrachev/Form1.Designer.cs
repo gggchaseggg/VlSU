c8b7341cb71a88deaa948f4f126e5c836a89e6c7
Daniil <gggchaseggg@gmail.com>
2023-10-14 15:13:15 +0300

    PKG-lab5

---
+++ PKG/lab5/lab5DaniilGrachev/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace lab5DaniilGrachev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.PointInGrap = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.RenderTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AnT
+            // 
+            this.AnT.AccumBits = ((byte)(0));
+            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoFinish = false;
+            this.AnT.AutoMakeCurrent = true;
+            this.AnT.AutoSwapBuffers = true;
+            this.AnT.BackColor = System.Drawing.Color.Black;
+            this.AnT.ColorBits = ((byte)(32));
+            this.AnT.DepthBits = ((byte)(16));
+            this.AnT.Location = new System.Drawing.Point(12, 12);
+            this.AnT.Name = "AnT";
+            this.AnT.Size = new System.Drawing.Size(653, 426);
+            this.AnT.StencilBits = ((byte)(0));
+            this.AnT.TabIndex = 0;
+            this.AnT.MouseMove += new System.Windows.Forms.MouseEventHandler(this.AnT_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(671, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Инициалы";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(671, 418);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 20);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // PointInGrap
+            // 
+            this.PointInGrap.Interval = 30;
+            this.PointInGrap.Tick += new System.EventHandler(this.PointInGrap_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(671, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Функция";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(671, 113);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 274);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.Value = 1000;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(714, 113);
+            this.trackBar2.Maximum = 1000;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 274);
+            this.trackBar2.TabIndex = 5;
+            this.trackBar2.TickFrequency = 10;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(761, 113);
+            this.trackBar3.Maximum = 1000;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar3.Size = new System.Drawing.Size(45, 274);
+            this.trackBar3.TabIndex = 6;
+            this.trackBar3.TickFrequency = 10;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(672, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "A             B              C";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(672, 390);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(720, 390);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(768, 390);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "0";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(671, 71);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Треугольник";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // RenderTimer
+            // 
+            this.RenderTimer.Interval = 25;
+            this.RenderTimer.Tick += new System.EventHandler(this.RenderTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AnT);
+            this.Name = "Form1";
+            this.Text = "Грачев Даниил Алексеевич ПРИ-120";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl AnT;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer PointInGrap;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer RenderTimer;
+    }
+}
+
---
+++ PKG/lab5/lab5DaniilGrachev/Form1.cs
@@ -0,0 +1,435 @@
+﻿using System;
+using System.Windows.Forms;
+// для работы с библиотекой OpenGL 
+using Tao.OpenGl;
+// для работы с библиотекой FreeGLUT 
+using Tao.FreeGlut;
+// для работы с элементом управления SimpleOpenGLControl 
+using Tao.Platform.Windows;
+using System.Reflection.Emit;
+
+namespace lab5DaniilGrachev
+{
+    public partial class Form1 : Form
+    {
+
+        double ScreenW, ScreenH;
+        private float devX;
+        private float devY;
+        private float[,] GrapValuesArray;
+        private int elements_count = 0;
+        private bool not_calculate = true;
+        private int pointPosition = 0;
+        float lineX, lineY;
+        float Mcoord_X = 0, Mcoord_Y = 0;
+
+        bool isFunctionsDisplay = false;
+        bool isTriDisplay = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AnT.InitializeContexts();
+        }
+
+        private void PointInGrap_Tick(object sender, EventArgs e)
+        {
+            if (isFunctionsDisplay) { 
+                if (pointPosition == elements_count - 1) pointPosition = 0;
+
+                Draw();
+
+                pointPosition++;
+            }
+        }
+
+        private void AnT_MouseMove(object sender, MouseEventArgs e)
+        {
+            Mcoord_X = e.X;
+            Mcoord_Y = e.Y;
+
+            lineX = devX * e.X;
+            lineY = (float)(ScreenH - devY * e.Y);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+
+            Gl.glClearColor(255, 255, 255, 1);
+
+            Gl.glViewport(0, 0, AnT.Width, AnT.Height);
+
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+
+            if ((float)AnT.Width <= (float)AnT.Height)
+            {
+                ScreenW = 30.0;
+                ScreenH = 30.0 * (float)AnT.Height / (float)AnT.Width;
+                Glu.gluOrtho2D(0.0, 30.0 * (float)AnT.Height / (float)AnT.Width, 0.0, 30.0);
+            } else {
+                ScreenW = 30.0 * (float)AnT.Width / (float)AnT.Height;
+                ScreenH = 30.0;
+                Glu.gluOrtho2D(0.0, 30.0 * (float)AnT.Width / (float)AnT.Height, 0.0, 30.0);
+            }
+
+            devX = (float)ScreenW / (float)AnT.Width;
+            devY = (float)ScreenH / (float)AnT.Height;
+
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+
+            // настройка параметров OpenGL для визуализации
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+            Gl.glEnable(Gl.GL_COLOR_MATERIAL);
+            PointInGrap.Start();
+            RenderTimer.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isFunctionsDisplay = false;
+            isTriDisplay = false;
+            // очищаем буфер цвета и глубины
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+
+            // очищаем текущую матрицу
+            Gl.glLoadIdentity();
+            // устанавливаем текущий цвет - красный
+            Gl.glColor3d(0.16, 0.67, 0.53);
+            Gl.glLineWidth((float)1.3);
+
+            // Г
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            Gl.glVertex2d(1, 29);
+            Gl.glVertex2d(1, 23);
+            Gl.glVertex2d(2, 23);
+            Gl.glVertex2d(2, 28);
+            Gl.glVertex2d(5, 28);
+            Gl.glVertex2d(5, 29);
+            Gl.glEnd();
+
+            // Р
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            Gl.glVertex2d(6, 29);
+            Gl.glVertex2d(6, 23);
+            Gl.glVertex2d(7, 23);
+            Gl.glVertex2d(7, 26);
+            Gl.glVertex2d(9, 26);
+            Gl.glVertex2d(9, 29);
+            Gl.glEnd();
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            Gl.glVertex2d(7, 28);
+            Gl.glVertex2d(7, 27);
+            Gl.glVertex2d(8, 27);
+            Gl.glVertex2d(8, 28);
+            Gl.glEnd();
+
+            // А
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            Gl.glVertex2d(10, 23);
+            Gl.glVertex2d(11, 29);
+            Gl.glVertex2d(13, 29);
+            Gl.glVertex2d(14, 23);
+            Gl.glVertex2d(13, 23);
+            Gl.glVertex2d(12.5, 25);
+            Gl.glVertex2d(11.5, 25);
+            Gl.glVertex2d(11, 23);
+            Gl.glEnd();
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            Gl.glVertex2d(11.5, 26);
+            Gl.glVertex2d(12.5, 26);
+            Gl.glVertex2d(12, 28);
+            Gl.glEnd();
+
+            // Ч
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            Gl.glVertex2d(15, 29);
+            Gl.glVertex2d(15, 26);
+            Gl.glVertex2d(17, 26);
+            Gl.glVertex2d(17, 23);
+            Gl.glVertex2d(18, 23);
+            Gl.glVertex2d(18, 29);
+            Gl.glVertex2d(17, 29);
+            Gl.glVertex2d(17, 27);
+            Gl.glVertex2d(16, 27);
+            Gl.glVertex2d(16, 29);
+            Gl.glEnd();
+
+            // Е
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            Gl.glVertex2d(19, 29);
+            Gl.glVertex2d(19, 23);
+            Gl.glVertex2d(22, 23);
+            Gl.glVertex2d(22, 24);
+            Gl.glVertex2d(20, 24);
+            Gl.glVertex2d(20, 26);
+            Gl.glVertex2d(22, 26);
+            Gl.glVertex2d(22, 27);
+            Gl.glVertex2d(20, 27);
+            Gl.glVertex2d(20, 28);
+            Gl.glVertex2d(22, 28);
+            Gl.glVertex2d(22, 29);
+            Gl.glEnd();
+
+            // В
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            Gl.glVertex2d(23, 29);
+            Gl.glVertex2d(23, 23);
+            Gl.glVertex2d(26, 23);
+            Gl.glVertex2d(26, 26);
+            Gl.glVertex2d(24, 26);
+            Gl.glVertex2d(24, 24);
+            Gl.glVertex2d(25, 24);
+            Gl.glVertex2d(25, 28);
+            Gl.glVertex2d(24, 28);
+            Gl.glVertex2d(24, 26);
+            Gl.glVertex2d(25.5, 26);
+            Gl.glVertex2d(25.5, 29);
+            Gl.glEnd();
+
+            // Д.
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            Gl.glVertex2d(28, 23);
+            Gl.glVertex2d(27, 24);
+            Gl.glVertex2d(27, 25);
+            Gl.glVertex2d(28.3, 25);
+            Gl.glVertex2d(29, 29);
+            Gl.glVertex2d(31, 29);
+            Gl.glVertex2d(31.7, 25);
+            Gl.glVertex2d(33, 25);
+            Gl.glVertex2d(33, 24);
+            Gl.glVertex2d(32, 23);
+            Gl.glVertex2d(31, 23);
+            Gl.glVertex2d(31.5, 24);
+            Gl.glVertex2d(28.5, 24);
+            Gl.glVertex2d(29, 23);
+            Gl.glEnd();
+            Gl.glEnd();
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            Gl.glVertex2d(29.5, 25);
+            Gl.glVertex2d(30.5, 25);
+            Gl.glVertex2d(30, 28);
+            Gl.glEnd();
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            Gl.glVertex2d(33.5, 23);
+            Gl.glVertex2d(34.5, 23);
+            Gl.glVertex2d(34.5, 24);
+            Gl.glVertex2d(33.5, 24);
+            Gl.glEnd();
+
+            // А.
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            Gl.glVertex2d(36, 23);
+            Gl.glVertex2d(37, 29);
+            Gl.glVertex2d(39, 29);
+            Gl.glVertex2d(40, 23);
+            Gl.glVertex2d(39, 23);
+            Gl.glVertex2d(38.5, 25);
+            Gl.glVertex2d(37.5, 25);
+            Gl.glVertex2d(37, 23);
+            Gl.glEnd();
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            Gl.glVertex2d(37.5, 26);
+            Gl.glVertex2d(38.5, 26);
+            Gl.glVertex2d(38, 28);
+            Gl.glEnd();
+            Gl.glEnd();
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            Gl.glVertex2d(40.5, 23);
+            Gl.glVertex2d(41.5, 23);
+            Gl.glVertex2d(41.5, 24);
+            Gl.glVertex2d(40.5, 24);
+            Gl.glEnd();
+
+            // дожидаемся конца визуализации кадра
+            Gl.glFlush();
+
+            // посылаем сигнал перерисовки элемента AnT.
+            AnT.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            isTriDisplay = false;
+            isFunctionsDisplay = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            isFunctionsDisplay = false;
+            isTriDisplay = true;
+        }
+
+        private void PrintText2D(float x, float y, string text)
+        {
+            Gl.glRasterPos2f(x, y);
+
+            foreach (char char_for_draw in text)
+            {
+                Glut.glutBitmapCharacter(Glut.GLUT_BITMAP_9_BY_15, char_for_draw);
+            }
+        }
+
+        double a = 1, b = 0, c = 0;
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            a = (double)trackBar1.Value / 1000.0;
+            label2.Text = a.ToString();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            b = (double)trackBar2.Value / 1000.0;
+            label3.Text = b.ToString();
+        }
+
+        private void RenderTimer_Tick(object sender, EventArgs e)
+        {
+            if (isTriDisplay)
+            {
+                DrawTri();
+            }
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            c = (double)trackBar3.Value / 1000.0;
+            label4.Text = c.ToString();
+        }
+
+        private void DrawTri()
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+
+            Gl.glBegin(Gl.GL_TRIANGLES);
+            Gl.glColor3d(0.0 + a, 0.0 + b, 0.0 + c);
+            Gl.glVertex2d(5.0, 5.0);
+            Gl.glColor3d(0.0 + c, 0.0 + a, 0.0 + b);
+            Gl.glVertex2d(25.0, 5.0);
+            Gl.glColor3d(0.0 + b, 0.0 + c, 0.0 + a);
+            Gl.glVertex2d(5.0, 25.0);
+            Gl.glEnd();
+
+            Gl.glFlush();
+
+            AnT.Invalidate();
+        }
+
+        private void functionCalculation()
+        {
+            float x = 0, y = 0;
+
+            GrapValuesArray = new float[300, 2];
+
+            elements_count = 0;
+
+            for (x = -5; x < 5; x += 0.5f)
+            {
+                y = (float) (Math.Pow(x,3)*Math.Exp(-Math.Abs(x+1)));
+
+                GrapValuesArray[elements_count, 0] = x;
+                GrapValuesArray[elements_count, 1] = y;
+                elements_count++;
+            }
+
+            not_calculate = false;
+        }
+
+        private void DrawDiagram()
+        {
+            if (not_calculate)
+            {
+                functionCalculation();
+            }
+
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+
+            Gl.glVertex2d(GrapValuesArray[0, 0], GrapValuesArray[0, 1]);
+
+            for (int ax = 1; ax < elements_count; ax += 2)
+            {
+                Gl.glVertex2d(GrapValuesArray[ax, 0], GrapValuesArray[ax, 1]);
+            }
+
+            Gl.glEnd();
+
+            Gl.glPointSize(5);
+            Gl.glColor3f(255, 0, 0);
+            Gl.glBegin(Gl.GL_POINTS);
+            Gl.glVertex2d(GrapValuesArray[pointPosition, 0], GrapValuesArray[pointPosition, 1]);
+            Gl.glEnd();
+            Gl.glPointSize(1);
+        }
+
+        private void Draw()
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+
+            Gl.glLoadIdentity();
+
+            Gl.glColor3f(0, 0, 0);
+
+            Gl.glPushMatrix();
+
+            Gl.glTranslated(15, 15, 0);
+
+            Gl.glBegin(Gl.GL_POINTS);
+
+            for (int ax = -15; ax < 15; ax++)
+            {
+                for (int bx = -15; bx < 15; bx++)
+                {
+                    Gl.glVertex2d(ax, bx);
+                }
+            }
+
+            Gl.glEnd();
+
+            Gl.glBegin(Gl.GL_LINES);
+            Gl.glVertex2d(0, -15);
+            Gl.glVertex2d(0, 15);
+            Gl.glVertex2d(-15, 0);
+            Gl.glVertex2d(15, 0);
+            Gl.glVertex2d(0, 15);
+            Gl.glVertex2d(0.1, 14.5);
+            Gl.glVertex2d(0, 15);
+            Gl.glVertex2d(-0.1, 14.5);
+            Gl.glVertex2d(15, 0);
+            Gl.glVertex2d(14.5, 0.1);
+            Gl.glVertex2d(15, 0);
+            Gl.glVertex2d(14.5, -0.1);
+            Gl.glEnd();
+
+            PrintText2D(15.5f, 0, "x");
+            PrintText2D(0.5f, 14.5f, "y");
+
+            DrawDiagram();
+
+            Gl.glPopMatrix();
+
+            PrintText2D(devX * Mcoord_X + 0.2f, (float)ScreenH - devY * Mcoord_Y + 0.4f, "[ x: " + (devX * Mcoord_X - 15).ToString() + " ; y: " + ((float)ScreenH - devY * Mcoord_Y - 15).ToString() + "]");
+
+            Gl.glColor3f(255, 0, 0);
+
+            Gl.glBegin(Gl.GL_LINES);
+            Gl.glVertex2d(lineX, 15);
+            Gl.glVertex2d(lineX, lineY);
+            Gl.glVertex2d(15, lineY);
+            Gl.glVertex2d(lineX, lineY);
+            Gl.glEnd();
+
+            Gl.glFlush();
+
+            AnT.Invalidate();
+        }
+    }
+}
